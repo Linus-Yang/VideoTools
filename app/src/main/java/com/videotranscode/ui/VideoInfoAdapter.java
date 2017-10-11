d12352ed87bee34f5aa427f258d9ea52b06d66f1
Linus-Yang <chyangbast@yeah.net>
2017-10-11 23:54:26 +0800

    [VideoTrancode] viwe init

--- app/src/main/java/com/videotranscode/ui/VideoInfoAdapter.java
+++ app/src/main/java/com/videotranscode/ui/VideoInfoAdapter.java
@@ -1,6 +1,8 @@
 package com.videotranscode.ui;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 /**
@@ -9,8 +11,11 @@ import android.view.ViewGroup;
 
 public class VideoInfoAdapter extends RecyclerView.Adapter {
 
+
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         return null;
     }
 
